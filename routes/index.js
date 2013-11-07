f585207bfcee7ec81e504452c7043b85e587ae0a
UFO <ufoqhmdt@gmail.com>
2013-11-07 18:48:54 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+
+var express = require('express');
+
+var app = express();
+
+app.get('/hello.txt', function(req, res) {
+	var body = 'Hello World';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
+});
+
+app.listen(3000);
+console.log('Listening on port 3000'); 
