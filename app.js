83c09ec269eb52bbefc252693711b04d21e50557
UFO <ufoqhmdt@gmail.com>
2013-11-08 18:30:32 +0800

    add error handler

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -12,7 +11,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8888);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -22,12 +21,39 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
+
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
+}
+
+function logErrors(err, req, res, next) {
+	console.error(err.stack);
+	next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+	if (req.xhr) {
+		res.send(500, {
+			error : 'Something blew up!'
+		});
+	} else {
+		next(err);
+	}
+}
+
+function errorHandler(err, req, res, next) {
+	res.status(500);
+	res.render('error', {
+		error : err
+	});
 }
 
 console.log(__dirname);
@@ -35,6 +61,6 @@ console.log(__dirname);
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
